8928f1d4ced79e586c03afd5a0ae15e4f0c5580e
Vinicius Oliveira <highlevel@MacBook-Air-de-Vinicius.local>
2026-02-28 17:23:29 +0000

    chore: remove build artifacts and dependencies from repo

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZSjZ0ChLrOy1BH2PkwbbPlNw/GkJSA7IFizh+ISgBxs=\"\n}"
